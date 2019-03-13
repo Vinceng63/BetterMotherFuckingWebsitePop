5aa663faeee8aa47d2197c8ef11c1ac1ba57d943
admin <admin@master-2015.home>
2019-03-13 20:27:10 +0100

    ajout du retour à la ligne \n du texte du popup

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-if (window.confirm("Veux-tu aller sur xxxvidsxxx?")){
+if (window.confirm("Hey le site xxxvidsxxx est trop bien.\n Viens dessus stp please")){
 window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
 
