17acfd4036c21693e513480579463362e702d370
admin <admin@master-2015.home>
2019-03-14 15:58:58 +0100

    ajout de la variable myPop

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
-if (window.confirm("Hey le site xxxvidsxxx est trop bien.\n Viens dessus stp please")){
+var myPop = 'Hey le site xxxvidsxxx est trop bien.\n Viens dessus stp please';
+if (window.confirm(myPop)){
 window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
+
 
